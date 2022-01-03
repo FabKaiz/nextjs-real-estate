41cb253f02a890884d9658294bb1cec563b52933
Fabien Chareun <cacrfabfr@gmail.com>
2022-01-03 12:55:12 +0100

    banner setupiin home component

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, i
       <Text color="gray.500" fontSize="sm" fontWeight="medium" >{purpose}</Text>
       <Text fontSize="3xl" fontWeight="bold" >{title1} <br /> {title2}</Text>
       <Text color="gray.700" fontSize="lg" paddingTop="3" paddingBottom="3" >{desc1}<br /> {desc2}</Text>
-      <Button fontSize="xl" bg="blue.300" color="white">
+      <Button fontSize="xl">
         <Link href={linkName}>{buttonText}</Link>
       </Button>
 
@@ -19,16 +19,31 @@ const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, i
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <Box>
       <Banner
-        purpose={'For Sale'}
-
-      /> 
+        purpose="RENT A HOME"
+        title1="Rental Homes for"
+        title2="Everyone"
+        desc1="Explore Apartments, Villas, Homes"
+        desc2="and more"
+        buttonText="Explore Renting"
+        linkName="/serch?purpose=for-rent"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+      />
+      <Flex flexWrap="wrap">
+        {/* fetch properties */}
+      </Flex>
       <Banner
-        purpose={'For Rent'}
-
-      /> 
-    </div>
+        purpose="BUY A HOME"
+        title1="Find, Buy & Own Your"
+        title2="Dream Home"
+        desc1="Explore Apartments, Villas, Homes"
+        desc2="and more"
+        buttonText="Explore Buying"
+        linkName="/serch?purpose=for-sale"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
+      />
+      {/* fetch properties */}
+    </Box>
   )
 }
