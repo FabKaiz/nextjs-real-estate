8cfce3c19b131fbfd6a7c3f89f8c4bc6b61a80c4
Fabien Chareun <cacrfabfr@gmail.com>
2022-01-03 14:14:08 +0100

    added layout component and structured app

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,24 @@
 
 import Router from 'next/router'
-import Header from 'next/head'
+import Head from 'next/head'
 import NProgress from 'nprogress'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+
+      </Head>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
+  )
 }
 
 export default MyApp
