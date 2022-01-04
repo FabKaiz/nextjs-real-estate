ff8501f32de7fd4c5d739dbe4928f901ed64f6d2
Fabien Chareun <cacrfabfr@gmail.com>
2022-01-04 14:55:50 +0100

    finished property details page

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -47,7 +47,7 @@ const ImageScrollbar = ({ data }) => (
           src={item.url}
           width={1000}
           height={500}
-          sizes="(max-width:500px) 100px, (max-height:1023px) 400px, 1000px"
+          sizes="(max-width:500px) 100px, (max-width:1023px) 400px, 1000px"
 
         />
 
