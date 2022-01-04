405e1fb5284929432f74afc38c5a5e49c29298a1
Fabien Chareun <cacrfabfr@gmail.com>
2022-01-04 13:43:28 +0100

    add search by filter

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         desc1="Explore Apartments, Villas, Homes"
         desc2="and more"
         buttonText="Explore Renting"
-        linkName="/serch?purpose=for-rent"
+        linkName="/search?purpose=for-rent"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       />
       <Flex flexWrap="wrap">
@@ -43,7 +43,7 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         desc1="Explore Apartments, Villas, Homes"
         desc2="and more"
         buttonText="Explore Buying"
-        linkName="/serch?purpose=for-sale"
+        linkName="/search?purpose=for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
       <Flex flexWrap="wrap">
